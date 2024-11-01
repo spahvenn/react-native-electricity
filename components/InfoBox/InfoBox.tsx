f336f755bac0c340457a1ff747020152fa721149
Sami Ahvenniemi <sami.ahvenniemi@valagroup.com>
2024-11-01 09:11:33 +0200

    Use Surface component to provide InfoBoxes some depth

--- components/InfoBox/InfoBox.tsx
+++ components/InfoBox/InfoBox.tsx
@@ -1,6 +1,6 @@
 import {FC} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {MD3Theme, Text, useTheme} from 'react-native-paper';
+import {Surface, Text, useTheme} from 'react-native-paper';
 
 type Props = {
   priceText: string;
@@ -9,9 +9,8 @@ type Props = {
 
 export const InfoBox: FC<Props> = ({priceText, bottomText}) => {
   const theme = useTheme();
-  const styles = makeStyles(theme);
   return (
-    <View style={styles.container}>
+    <Surface style={styles.container} elevation={2}>
       <View>
         <View style={styles.upperTextContainer}>
           <Text style={styles.priceText}>{priceText}</Text>
@@ -21,35 +20,34 @@ export const InfoBox: FC<Props> = ({priceText, bottomText}) => {
           <Text>{bottomText}</Text>
         </View>
       </View>
-    </View>
+    </Surface>
   );
 };
 
-const makeStyles = (theme: MD3Theme) =>
-  StyleSheet.create({
-    container: {
-      margin: 8,
-      paddingVertical: 12,
-      backgroundColor: theme.colors.elevation.level3,
-      borderRadius: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    upperTextContainer: {
-      flexDirection: 'row',
-      alignItems: 'flex-end',
-      justifyContent: 'center',
-      marginBottom: 4,
-    },
-    priceText: {
-      fontSize: 24,
-      lineHeight: 24,
-      marginRight: 2,
-    },
-    unitText: {
-      fontSize: 18,
-      lineHeight: 24,
-      marginBottom: 2,
-    },
-    lowerTextContainer: {justifyContent: 'center', flexDirection: 'row'},
-  });
+const styles = StyleSheet.create({
+  container: {
+    margin: 8,
+    marginBottom: 12,
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  upperTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    marginBottom: 4,
+  },
+  priceText: {
+    fontSize: 24,
+    lineHeight: 24,
+    marginRight: 2,
+  },
+  unitText: {
+    fontSize: 18,
+    lineHeight: 24,
+    marginBottom: 2,
+  },
+  lowerTextContainer: {justifyContent: 'center', flexDirection: 'row'},
+});
