b3f784bd49a16587cc8ac3a774edc330edb45fcf
Sami Ahvenniemi <sami.ahvenniemi@valagroup.com>
2024-11-04 09:09:29 +0200

    Change statusbar text color when in dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import {Stack} from 'expo-router';
 import {useStore} from '@/store/store';
 import {DefaultTheme, MD3DarkTheme, PaperProvider} from 'react-native-paper';
 import {Settings} from 'luxon';
+import {StatusBar} from 'expo-status-bar';
 
 Settings.defaultZone = 'Europe/Helsinki';
 
@@ -9,6 +10,7 @@ export default function RootLayout() {
   const {isDarkMode} = useStore();
   return (
     <PaperProvider theme={isDarkMode ? MD3DarkTheme : DefaultTheme}>
+      <StatusBar style={isDarkMode ? 'light' : 'dark'} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{headerShown: false}} />
         <Stack.Screen name="+not-found" />
